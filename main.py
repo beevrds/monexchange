dd303ac41a74ba8eb70af476b4527a5073c8f2da
dsjin <s7070053@kmitl.ac.th>
2014-11-11 22:33:46 +0700

    Create Main file of project

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Program: monexchange
+Author: Mathutin and Thatchakon
+Version: Alpha 0.1
+Date Modifile: 11/11/2014 22.31 PM
+Detail: Currency Exchanging Program by input number of money and
+        select input's currency and another one currency which
+        you want to exchange.
+'''
