8d6f609e381775256ae21523e473328f57275861
dsjin <s7070053@kmitl.ac.th>
2014-11-11 22:37:11 +0700

    Edit Docsting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Program: monexchange
 Author: Mathutin and Thatchakon
 Version: Alpha 0.1
-Date Modifile: 11/11/2014 22.31 PM
+Date modified: 11/11/2014 22.31 PM
 Detail: Currency Exchanging Program by input number of money and
         select input's currency and another one currency which
         you want to exchange.
