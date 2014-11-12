2e6da436fb002deebbb1b6a47e28ece127c3d512
Mathurin1996 <s7070094@kmitl.ac.th>
2014-11-12 20:43:20 +0700

    Edit Name & Student ID
    editt at line 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''
 Program: monexchange
-Author: Mathutin and Thatchakon
+Author: Mathurin (094) and Thatchakon (053)
 Version: Alpha 0.1
 Date modified: 11/11/2014 22.31 PM
 Detail: Currency Exchanging Program by input number of money and
