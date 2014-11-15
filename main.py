a77156f4294ee2f6e6868ffae0f979708ef3229e
dsjin <s7070053@kmitl.ac.th>
2014-11-15 11:43:47 +0700

    Add Codes : Connect to API, Main GUI

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 '''
 Program: monexchange
 Author: Mathurin (094) and Thatchakon (053)
-Version: Alpha 0.1
-Date modified: 11/11/2014 22.31 PM
+Version: Alpha 1.0
+Date modified: 15/11/2014 11.41 AM
 Detail: Currency Exchanging Program by input number of money and
         select input's currency and another one currency which
         you want to exchange.
+API : http://www.freecurrencyconverterapi.com/api
 '''
+import json
+import urllib2
+from Tkinter import *
+import tkMessageBox
+class connection:
+    def __init__(self):
+        try:
+            self.country = json.load(urllib2.urlopen('http://www.freecurrencyconverterapi.com/api/v2/countries'))
+        except:
+            This_mess = tkMessageBox.showerror('Error!', 'Can\'t connect to API.')
+            if This_mess == 'ok':
+                main.destroy()
+class app:
+    def __init__(self, main):
+        #Edit GUI Here
+        self.textwelcome = Label(main, text = 'monexchange')
+        
+main = Tk()
+main.title('monexchange')
+main.geometry('400x400')
+mainGUI = app(main)
+mainGUI.textwelcome.pack()
+mainConnect = connection()
+main.mainloop()
