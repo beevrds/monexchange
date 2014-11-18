d55d3af7e603b14ef514d2a7a6a0431ce206e58b
Mathurin1996 <s7070094@kmitl.ac.th>
2014-11-18 20:22:06 +0700

    Add Codes & Edit Class Name

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import json
 import urllib2
 from Tkinter import *
 import tkMessageBox
-class connection:
+class Connection:
     def __init__(self):
         try:
             self.country = json.load(urllib2.urlopen('http://www.freecurrencyconverterapi.com/api/v2/countries'))
@@ -20,15 +20,27 @@ class connection:
             This_mess = tkMessageBox.showerror('Error!', 'Can\'t connect to API.')
             if This_mess == 'ok':
                 main.destroy()
-class app:
+class App:
     def __init__(self, main):
         #Edit GUI Here
+        self.values()
         self.textwelcome = Label(main, text = 'monexchange')
-        
+        self.fill1 = self.textfill(self.money1)
+        self.fill2 = self.textfill(self.money2)
+        self.button = Button(main, text = 'OK', command = '#command#')
+    def values(self):
+        self.money1 = IntVar()
+        self.money2 = IntVar()
+    def textfill(self, var_money):
+        return Entry(main, textvariable = var_money)
 main = Tk()
 main.title('monexchange')
-main.geometry('400x400')
-mainGUI = app(main)
+main.geometry('400x600')
+values = StringVar
+values2 = StringVar
+mainGUI = App(main)
 mainGUI.textwelcome.pack()
-mainConnect = connection()
+mainGUI.fill1.pack()
+mainGUI.fill2.pack()
+mainConnect = Connection()
 main.mainloop()
