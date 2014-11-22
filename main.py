58608e3d7035b9ba781c388f0e80ba5871bd9fb3
dsjin <s7070053@kmitl.ac.th>
2014-11-22 11:07:44 +0700

    Create Func. Calculate

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Program: monexchange
 Author: Mathurin (094) and Thatchakon (053)
 Version: Alpha 2.0
-Date modified: 22/11/2014 09.10 AM
+Date modified: 22/11/2014 10.41 AM
 Detail: Currency Exchanging Program by input number of money and
         select input's currency and another one currency which
         you want to exchange.
@@ -20,12 +20,13 @@ class Connection:
             self.mess = 'Pass'
         except:
             self.mess = tkMessageBox.showerror('Error!', 'Can\'t connect to API.')
+    def thisrate(self, val1, val2):
+        return json.load(urllib2.urlopen('http://www.freecurrencyconverterapi.com/api/v2/convert?q=%s_%s&compact=y' % (val1, val2)))
 class App:
     def __init__(self, main):
         #Edit GUI Here
         #Label
         self.text = self.textlabel(main, 'TO ', '#A9742B')
-        self.text.config(font = 'Lato')
         #Fillbox
         self.fill1 = self.textfill(value.money1)
         self.fill1.config(width=40)
@@ -35,7 +36,7 @@ class App:
         self.select2 = self.select(value.curr2, country)
         self.select2.config(width=20)
         #Button
-        self.button = Button(main, text = 'EXCHANGE !', command = self.printt)
+        self.button = Button(main, text = 'EXCHANGE !', command = self.calculate)
         self.button.config(height = 2, width = 10)
     def textfill(self, var_money):
         #Create Text Fill <Entry>
@@ -43,22 +44,25 @@ class App:
     def select(self, inputvalues, list_country):
         #Create Dropdown Menu
         return apply(OptionMenu, (main, inputvalues) + tuple(list_country))
-    def textlabel(self, main, your_text, color):
+    def textlabel(self, main, your_text, color = '#A9742B'):
         #Create Label
         return Label(main, text = your_text, bg = color)
-    def printt(self):
-        '''Test Button'''
-        temp1 = value.money1.get()
-        temp2 = value.curr1.get()
-        temp3 = value.curr2.get()
-        temp4 = json.load(urllib2.urlopen('http://www.freecurrencyconverterapi.com/api/v2/convert?q=%s_%s&compact=y' % (short[temp2], short[temp3])))
+    def calculate(self):
+        #Calculate money exchange
+        money1 = value.money1.get()
+        current1 = value.curr1.get()
+        current2 = value.curr2.get()
+        rate = mainConnect.thisrate(short[current1], short[current2])
+        present = float(money1)*rate['%s_%s' % (short[current1], short[current2])]['val']
+        self.text1 = self.textlabel(main, present)
+        self.text1.place(x = 150, y = 350)
 class Allvalues:
     '''
     Set All Values
     '''
     def __init__(self):
         #All of Values in Program
-        self.money1 = IntVar()
+        self.money1 = StringVar()
         self.curr1 = StringVar()
         self.curr2 = StringVar()
 def guipack():
@@ -73,8 +77,9 @@ def addcountry():
     country = list()
     short_form = dict()
     for i in mainConnect.country['results'].keys():
-        country.append(mainConnect.country['results'][i]['currencyName'])
-        short_form[mainConnect.country['results'][i]['currencyName']] = mainConnect.country['results'][i]['currencyId']
+        if mainConnect.country['results'][i]['currencyName'] not in country:
+            country.append(mainConnect.country['results'][i]['currencyName'])
+            short_form[mainConnect.country['results'][i]['currencyName']] = mainConnect.country['results'][i]['currencyId']
     return (country, short_form)
 main = Tk()
 main.title('monexchange')
